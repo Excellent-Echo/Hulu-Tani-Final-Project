eccf74095a58cb40374a46113fba93806a0630dc
rizkyran <rizkyran.rr@gmail.com>
2021-06-18 17:34:15 +0700

    add user pages

---
+++ hulutani-client/src/pages/user/login.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import '../../assets/css/auth.css'
+
+import AuthShape from '../../assets/images/auth-bg-shape.svg'
+import LoginIllustration from '../../assets/images/login-img.svg'
+
+function Login() {
+    return (
+        <>
+            <div className="container-fluid fluid-page auth-page-container">
+                <img src={ AuthShape } alt="" className="img-fluid auth-bg-shape login" />
+                <div className="container">
+                    <div className="row justify-content-center auth-container">
+                        <div className="col-sm content-container">
+                            <div className="row">
+                                <h1 className="title text-center">
+                                    Masuk dengan akunmu
+                                </h1>
+                            </div>
+                            <div className="row">
+                            <form>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                    <p className="subtitle small text-end form-helper-text">Lupa Password</p>
+                                </div>
+                                <button type="submit" className="primary long">Masuk</button>
+                            </form>
+                                <p className="small text-center">
+                                    Belum punya akun?&nbsp;
+                                    <b className="form-helper-text">
+                                        Daftar
+                                    </b>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-sm img-container">
+                            <img src={ LoginIllustration } alt="" className="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login

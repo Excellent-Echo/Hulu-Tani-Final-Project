d34de6b06256ec27a2098fddfa802fba1fd666e5
ardhisr1 <radika.kerenz1@gmail.com>
2021-06-18 16:57:02 +0700

    add user register redux with dummy data

---
+++ hulutani-client/src/redux/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import userRegisterReducer from "./user/register/userRegisterReducer";
+import userLoginReducer from "./user/login/userLoginReducer"
+
+const rootReducer = combineReducers({
+    userRegister: userRegisterReducer,
+    userLogin: userLoginReducer,
+})
+
+export default rootReducer;

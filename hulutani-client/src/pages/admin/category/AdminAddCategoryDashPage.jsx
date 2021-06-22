c817e59deb9e0379643cb550edc90d8fdb61fc98
efzedel16 <efzedel16@outlook.com>
2021-06-22 21:39:59 +0700

    Fix add product & category

--- hulutani-client/src/pages/admin/category/AdminAddCategoryDashPage.jsx
+++ hulutani-client/src/pages/admin/category/AdminAddCategoryDashPage.jsx
@@ -1,56 +1,60 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import adminAddCategoryAction from "../../../redux/admin/category/add/adminAddCategoryAction"
-import adminShowCategoryAction from "../../../redux/admin/category/show/adminShowCategoryAction"
+import adminAddCategoryAction from "../../../redux/admin/category/add/adminAddCategoryAction";
+import adminShowCategoryAction from "../../../redux/admin/category/show/adminShowCategoryAction";
 
 import HeaderAdmin from "../../../components/organisms/admin/HeaderAdmin/HeaderAdmin";
 import SideAdminNavBar from "../../../components/organisms/admin/SideNavBar/SideAdminNavBar";
 
 const AdminAddCategoryDashPage = () => {
-  const categoryData = useSelector(state => state.adminAddCategory)
-  const dispatch = useDispatch()
-  const history = useHistory()
+  const categoryData = useSelector((state) => state.adminAddCategory);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const addCategoryHandler = (e) => {
-    e.preventDefault()
-    dispatch(adminAddCategoryAction.addCategory(
-      categoryData.categoryName
-    ))
-    dispatch(adminShowCategoryAction.getCategories())
-    history.push("/admin/dash")
-  }
+    e.preventDefault();
+    dispatch(adminAddCategoryAction.addCategory(categoryData.categoryName));
+    dispatch(adminShowCategoryAction.getCategories());
+    history.push("/admin/dash");
+  };
 
   return (
     <div className="user-select-none">
       <HeaderAdmin />
       <SideAdminNavBar />
-        <div className="h-100 w-100 px-4">
-          <div className="h-75 ahdp_recent">
-            <h3 className="h-25 d-flex align-items-center">
-              Kategori &gt; Tambah
-            </h3>
 
-            <form className="bg-white p-3 rounded-3" onSubmit={addCategoryHandler}>
-              {/* category name */}
-              <div className="mb-3 row">
-                <label
-                  htmlFor="inputCategoryName"
-                  className="col-sm-2 col-form-label"
-                >
-                  Nama
-                </label>
-                <div className="col-sm-10">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="inputCategoryName"
-                    placeholder="Enter product name"
-                    value={categoryData.categoryName}
-                    onChange={e => dispatch(adminAddCategoryAction.setCategory(e.target.value))}
-                  />
-                </div>
+      <div className="admin-content-container">
+        <div className="h-75 ahdp_recent">
+          <h3 className="h-25 d-flex align-items-center">
+            Kategori &gt; Tambah
+          </h3>
+
+          <form
+            className="bg-white p-3 rounded-3"
+            onSubmit={addCategoryHandler}
+          >
+            {/* category name */}
+            <div className="mb-3 row">
+              <label
+                htmlFor="inputCategoryName"
+                className="col-sm-2 col-form-label"
+              >
+                Nama
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputCategoryName"
+                  placeholder="Enter product name"
+                  value={categoryData.categoryName}
+                  onChange={(e) =>
+                    dispatch(adminAddCategoryAction.setCategory(e.target.value))
+                  }
+                />
               </div>
+            </div>
             {/* category name */}
 
             <Link to="/admin/dash/category">

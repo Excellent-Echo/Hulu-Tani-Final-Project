86f9b4d4f0ae890ddc25e110fd9a08ea064d2295
rizkyran <rizkyran.rr@gmail.com>
2021-06-24 16:45:40 +0700

    done styling sidebar produk sementara

--- hulutani-client/src/pages/user/produklist.js
+++ hulutani-client/src/pages/user/produklist.js
@@ -20,75 +20,81 @@ function ProdukList() {
 								<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 								<span class="navbar-toggler-icon"></span>
 								</button>
-								<div class="collapse navbar-collapse" id="navbarSupportedContent">
-									<ul class="navbar-nav mx-4 mb-lg-0">
-										<li class="nav-item dropdown">
-											<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-												Kategori
-											</a>
-											<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-												<li><a class="dropdown-item" href="#">Action</a></li>
-												<li><a class="dropdown-item" href="#">Another action</a></li>
+								<div class="container-fluid collapse navbar-collapse" id="navbarSupportedContent">
+									<div className="row justify-content-center">
+										<div className="col-sm-4 d-flex justify-self-end">
+											<ul class="navbar-nav">
+												<li class="nav-item dropdown">
+													<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+														Kategori
+													</a>
+													<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+														<li><a class="dropdown-item" href="#">Action</a></li>
+														<li><a class="dropdown-item" href="#">Another action</a></li>
+													</ul>
+												</li>
 											</ul>
-										</li>
-									</ul>
-									<form class="">
-										<input class="form-control nav-search long" type="search" placeholder="Cari 'wortel'" aria-label="Search"/>
-									</form>
-									{/* <div class="navbar-nav ms-2">
-										
-									</div> */}
+										</div>
+										<div className="col-sm-8">
+											<form class="">
+												<input class="form-control nav-search" type="search" placeholder="Cari 'wortel'" aria-label="Search"/>
+											</form>
+										</div>
+									</div>
 								</div>
 							</div>
 						</nav>
 					</div>
 					<div className="container-fluid">
 						<div className="container">
-							<div className="col-sm-2 filter-sidebar-container">
-								<div className="container-fluid filter-sidebar">
-									<div className="row sidebar-title">
+							<div className="filter-sidebar-container">
+								<div className="col-sm-2 filter-sidebar">
+									<div className="container-fluid sidebar-title">
 										<h4 className="title text-center">
 											FILTER PRODUK
 										</h4>
 									</div>
-									<div className="row sidebar-content">
-										<div className="row filter">
-											<h6 className="accent-text accent-title">
-												KATEGORI
-											</h6>
-											<ul className="list-unstyled">
-												<li>
-													<a href="">Sayuran</a>
-												</li>
-												<li>
-													<a href="">Buah</a>
-												</li>
-												<li>
-													<a href="">Olahan Pangan</a>
-												</li>
-												<li>
-													<a href="">Ikan</a>
-												</li>
-											</ul>
-										</div>
-										<div className="row filter">
-											<h6 className="accent-text accent-title">
-												HARGA
-											</h6>
-											<ul className="list-unstyled">
-												<li>
-													<a href=""> Rp. 50k</a>
-												</li>
-												<li>
-													<a href="">Buah</a>
-												</li>
-												<li>
-													<a href="">Olahan Pangan</a>
-												</li>
-												<li>
-													<a href="">Ikan</a>
-												</li>
-											</ul>
+									<div className="container-fluid sidebar-content-container">
+										<br />
+										<div className="row sidebar-content">
+											<div className="row filter">
+												<h6 className="accent-text accent-title">
+													KATEGORI
+												</h6>
+												<ul className="list-unstyled">
+													<li>
+														<a href="">Sayuran</a>
+													</li>
+													<li>
+														<a href="">Buah</a>
+													</li>
+													<li>
+														<a href="">Olahan Pangan</a>
+													</li>
+													<li>
+														<a href="">Ikan</a>
+													</li>
+												</ul>
+											</div>
+											<div className="row filter">
+												<h6 className="accent-text accent-title">
+													HARGA
+												</h6>
+												<ul className="list-unstyled">
+													<li>
+														<a href="">dibawah Rp 50k</a>
+													</li>
+													<li>
+														<a href="">diatas Rp 50k</a>
+													</li>
+													<li>
+														<a href="">dibawah Rp 100k</a>
+													</li>
+													<li>
+														<a href="">diatas Rp 100k</a>
+													</li>
+												</ul>
+											</div>
 										</div>
 									</div>
 								</div>
@@ -112,7 +118,7 @@ function ProdukList() {
 										{catalogProduct.allProduct.map((data,index)=>{
 											return (
 											<div className="col-sm card-container" key={index}>
-												<div className="card product-card card-outline-primary h-100" style={{ width: "12rem" }}>
+												<div className="card product-card card-outline-primary h-100" style={{ width: "11.8rem" }}>
 													<span className="badge product-btn-wl b-transparent">
 														<i class="far fa-heart fa-2x"></i>
 													</span>

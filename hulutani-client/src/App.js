dd5f16a811cabcdbe8563ce77698d7e0da3546bb
efzedel16 <efzedel16@outlook.com>
2021-06-26 18:38:19 +0700

    Fix

--- hulutani-client/src/App.js
+++ hulutani-client/src/App.js
@@ -5,38 +5,36 @@ import PrivateRoute from "./components/route/PrivateRoute";
 //Public
 import LandingPage from "./pages/user/landing";
 import ProdukList from "./pages/user/produklist";
-import ProdukDetail from "./pages/user/produkdetail"
+import ProdukDetail from "./pages/user/produkdetail";
 import Login from "./pages/user/login";
 import Register from "./pages/user/register";
-import RegisterNext from "./pages/user/registernext"
+import RegisterNext from "./pages/user/registernext";
 
 //User
 import UserEdit from "./pages/user/useredit";
 import UserTransaksi from "./pages/user/usertransaksi";
 import UserAlamat from "./pages/user/useralamat";
-import KantongBelanja from "./pages/user/kantongbelanja"
+import KantongBelanja from "./pages/user/kantongbelanja";
 
 //Admin
-import AdminLogin from "./pages/admin/AdminLoginPage";
 import AdminHome from "./pages/admin/home/AdminHomeDashPage";
-// import AdminProduct from "./pages/admin/product/AdminProductDashPage";
-// import AdminAddProduct from "./pages/admin/product/AdminAddProductDashPage";
-// import AdminEditProduct from "./pages/admin/product/adminEditProductDashPage";
-// import AdminCategory from "./pages/admin/category/AdminCategoryDashPage";
-// import AdminAddCategory from "./pages/admin/category/AdminAddCategoryDashPage";
-// import AdminEditCategory from "./pages/admin/category/AdminEditCategoryDashPage";
-// import AdminTransaction from "./pages/admin/transaction/AdminTransactionDashPage";
-// import AdminTransactionDetail from "./pages/admin/transaction/AdminTransactionDetailDashPage";
-// import AdminContact from "./pages/admin/contact/AdminContactDashPage";
+import AdminProduct from "./pages/admin/product/AdminProductDashPage";
+import AdminAddProduct from "./pages/admin/product/AdminAddProductDashPage";
+import AdminEditProduct from "./pages/admin/product/adminEditProductDashPage";
+import AdminCategory from "./pages/admin/category/AdminCategoryDashPage";
+import AdminAddCategory from "./pages/admin/category/AdminAddCategoryDashPage";
+import AdminEditCategory from "./pages/admin/category/AdminEditCategoryDashPage";
+import AdminTransaction from "./pages/admin/transaction/AdminTransactionDashPage";
+import AdminTransactionDetail from "./pages/admin/transaction/AdminTransactionDetailDashPage";
+import AdminContact from "./pages/admin/contact/AdminContactDashPage";
 
 //test
-import ImageUploadTest from "./pages/ImageUploadTest"
+import ImageUploadTest from "./pages/ImageUploadTest";
 
 function App() {
   return (
     <Router>
       <Switch>
-
         {/* User Routes */}
         <Route path="/login">
           <Login />
@@ -57,50 +55,49 @@ function App() {
           <KantongBelanja />
         </Route>
         {/* Private */}
-        <PrivateRoute path="/user-edit" component={UserEdit}/>
-        <PrivateRoute path="/user-transaction" component={UserTransaksi}/>
-        <PrivateRoute path="/user-address" component={UserAlamat}/>
-        <PrivateRoute path="/cart" component={KantongBelanja}/>
+        <PrivateRoute path="/user-edit" component={UserEdit} />
+        <PrivateRoute path="/user-transaction" component={UserTransaksi} />
+        <PrivateRoute path="/user-address" component={UserAlamat} />
+        <PrivateRoute path="/cart" component={KantongBelanja} />
         {/* Private */}
 
         {/* User Routes */}
 
         {/* Admin Routes */}
-        <Route component={AdminLogin} exact path="/admin/login" />
-        <Route component={AdminHome} exact path="/admin/dash" />
-        <Route component={AdminProduct} exact path="/admin/dash/product" />
+        <Route exact path="/admin/dash" component={AdminHome} />
+        <Route exact path="/admin/dash/product" component={AdminProduct} />
         <Route
-          component={AdminAddProduct}
           exact
           path="/admin/dash/product/add"
+          component={AdminAddProduct}
         />
         <Route
-          component={AdminEditProduct}
           exact
           path="/admin/dash/product/edit/:id"
+          component={AdminEditProduct}
         />
-        <Route component={AdminCategory} exact path="/admin/dash/category" />
+        <Route exact path="/admin/dash/category" component={AdminCategory} />
         <Route
-          component={AdminAddCategory}
           exact
           path="/admin/dash/category/add"
+          component={AdminAddCategory}
         />
         <Route
-          component={AdminEditCategory}
           exact
           path="/admin/dash/category/edit/:id"
+          component={AdminEditCategory}
         />
         <Route
-          component={AdminTransaction}
           exact
           path="/admin/dash/transaction"
+          component={AdminTransaction}
         />
         <Route
-          component={AdminTransactionDetail}
           exact
           path="/admin/dash/transaction/detail"
+          component={AdminTransactionDetail}
         />
-        <Route component={AdminContact} exact path="/admin/dash/contact" />
+        <Route exact path="/admin/dash/contact" component={AdminContact} />
         {/* Admin Routes */}
 
         <Route path="/test">

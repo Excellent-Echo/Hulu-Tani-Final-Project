29792b82255a04d648fef831092842784728949c
efzedel16 <efzedel16@outlook.com>
2021-06-26 15:16:20 +0700

    Fix admin header

--- hulutani-client/src/components/organisms/admin/AdminHeader.jsx
+++ hulutani-client/src/components/organisms/admin/AdminHeader.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const AdminHeader = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="navbar navbar-light bg-light position-sticky top-0 admin-header shadow-sm">
+      <div className="container justify-content-end">
+        <div className="navbar-brand d-flex flex-row-reverse" href="#">
+          <div className="fs-1 ms-3">
+            <i className="far fa-user-circle"></i>
+          </div>
+          <span className="d-flex align-items-center">Hai, Admin!</span>
+        </div>
+      </div>
+    </nav>
   );
 };
 

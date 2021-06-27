d4d22bb6fb3b779f3f45929521f44595708279d0
efzedel16 <efzedel16@outlook.com>
2021-06-28 01:39:48 +0700

    Fix duplicate editCategory in line 101

--- hulutani-client/src/redux/admin/category/edit/adminEditCategoryAction.js
+++ hulutani-client/src/redux/admin/category/edit/adminEditCategoryAction.js
@@ -1,105 +1,108 @@
-import hulutaniClient from "../../../../APIs/hulutaniClient"
-import { ADMIN_UPDATE_CATEGORY_SET_CATEGORY, ADMIN_UPDATE_CATEGORY_SET_ERROR_MESSAGE, ADMIN_UPDATE_CATEGORY_SET_ID_CATEGORY, ADMIN_UPDATE_CATEGORY_SET_SUCCESS_MESSAGE, ADMIN_UPDATE_CATEGORY_START_LOADING, ADMIN_UPDATE_CATEGORY_STOP_LOADING } from "../../actionType"
-import adminShowCategoryAction from "../show/adminShowCategoryAction"
+import hulutaniClient from "../../../../APIs/hulutaniClient";
+import {
+  ADMIN_UPDATE_CATEGORY_SET_CATEGORY,
+  ADMIN_UPDATE_CATEGORY_SET_ERROR_MESSAGE,
+  ADMIN_UPDATE_CATEGORY_SET_ID_CATEGORY,
+  ADMIN_UPDATE_CATEGORY_SET_SUCCESS_MESSAGE,
+  ADMIN_UPDATE_CATEGORY_START_LOADING,
+  ADMIN_UPDATE_CATEGORY_STOP_LOADING,
+} from "../../actionType";
+import adminShowCategoryAction from "../show/adminShowCategoryAction";
 
-const setCategoryID = categoryID => {
-    return {
-        type : ADMIN_UPDATE_CATEGORY_SET_ID_CATEGORY,
-        payload : {
-            categoryID : categoryID
-        }
-    }
-}
-
-const setCategory = categoryName => {
-    return {
-        type : ADMIN_UPDATE_CATEGORY_SET_CATEGORY,
-        payload : {
-            categoryName: categoryName
-        }
-    }
-}
-
-const setErrorMessage = errorMessage => {
-    return {
-        type: ADMIN_UPDATE_CATEGORY_SET_ERROR_MESSAGE,
-        payload: {
-            errorMessage: errorMessage,
-        },
-    };
+const setCategoryID = (categoryID) => {
+  return {
+    type: ADMIN_UPDATE_CATEGORY_SET_ID_CATEGORY,
+    payload: {
+      categoryID: categoryID,
+    },
+  };
 };
 
-const setSuccessMessage = successMessage => {
-    return {
-        type: ADMIN_UPDATE_CATEGORY_SET_SUCCESS_MESSAGE,
-        payload: {
-            successMessage: successMessage,
-        },
-    };
+const setCategory = (categoryName) => {
+  return {
+    type: ADMIN_UPDATE_CATEGORY_SET_CATEGORY,
+    payload: {
+      categoryName: categoryName,
+    },
+  };
+};
+
+const setErrorMessage = (errorMessage) => {
+  return {
+    type: ADMIN_UPDATE_CATEGORY_SET_ERROR_MESSAGE,
+    payload: {
+      errorMessage: errorMessage,
+    },
+  };
+};
+
+const setSuccessMessage = (successMessage) => {
+  return {
+    type: ADMIN_UPDATE_CATEGORY_SET_SUCCESS_MESSAGE,
+    payload: {
+      successMessage: successMessage,
+    },
+  };
 };
 
 const startLoading = () => {
-    return {
-        type: ADMIN_UPDATE_CATEGORY_START_LOADING,
-    };
+  return {
+    type: ADMIN_UPDATE_CATEGORY_START_LOADING,
+  };
 };
 
 const stopLoading = () => {
-    return {
-        type: ADMIN_UPDATE_CATEGORY_STOP_LOADING,
-    };
+  return {
+    type: ADMIN_UPDATE_CATEGORY_STOP_LOADING,
+  };
 };
 
-const getCategory = (id, categoryName) => async dispatch => {
-    try {
-        console.log("updating category..")
-        dispatch(startLoading())
+const getCategory = (id, categoryName) => async (dispatch) => {
+  try {
+    console.log("updating category..");
+    dispatch(startLoading());
 
-        const res = await hulutaniClient({
-            method: "GET",
-            url: `/kategori/${id}`,
-        })
+    const res = await hulutaniClient({
+      method: "GET",
+      url: `/kategori/${id}`,
+    });
 
-        dispatch(setCategory(res.data.data.nama))
-        dispatch(adminShowCategoryAction.getCategories())
-        dispatch(stopLoading())
-        console.log("success updating data")
-    } catch (error) {
-        console.log(error)
-    }
-}
+    dispatch(setCategory(res.data.data.nama));
+    dispatch(adminShowCategoryAction.getCategories());
+    dispatch(stopLoading());
+    console.log("success updating data");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const editCategory = (id, categoryName) => async dispatch => {
-    try {
-        console.log("updating category..")
-        dispatch(startLoading())
+const editCategory = (id, categoryName) => async (dispatch) => {
+  try {
+    console.log("updating category..");
+    dispatch(startLoading());
 
-        const updateData = {
-            nama: categoryName
-        }
+    const updateData = {
+      nama: categoryName,
+    };
 
-        const update = await hulutaniClient({
-            method: "PUT",
-            url: `/kategori/${id}`,
-            data: updateData
-        })
-        
+    const update = await hulutaniClient({
+      method: "PUT",
+      url: `/kategori/${id}`,
+      data: updateData,
+    });
 
-        dispatch(stopLoading())
-        console.log("success updating data")
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
+    dispatch(stopLoading());
+    console.log("success updating data");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const adminEditCategoryAction = {
-    setCategory,
-    setCategoryID,
-    editCategory,
-    editCategory,
-    getCategory
-}
+  setCategory,
+  setCategoryID,
+  editCategory,
+  getCategory,
+};
 
 export default adminEditCategoryAction;
